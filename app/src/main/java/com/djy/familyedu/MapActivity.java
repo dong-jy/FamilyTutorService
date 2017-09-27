1b7e3b78c9cc7e6650993d4b6e93865595b0539b
dong jiangying <krivergo3@gmail.com>
2017-09-27 20:21:50 +0800

    try to  add poi search, failed

--- app/src/main/java/com/djy/familyedu/MapActivity.java
+++ app/src/main/java/com/djy/familyedu/MapActivity.java
@@ -1,12 +1,20 @@
 package com.djy.familyedu;
 
 import android.Manifest;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SearchRecentSuggestionsProvider;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -19,32 +27,61 @@ import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
-import com.amap.api.services.poisearch.PoiSearch;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 
-public class MapActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener
-        ,AMapLocationListener,LocationSource {
+public class MapActivity extends AppCompatActivity implements AMapLocationListener
+        , CompoundButton.OnCheckedChangeListener, LocationSource {
 
     protected static final String TAG = "MapActivity";
+    public AMapLocationClient mlocationClient;
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
 
+        }
+    };
+    MaterialSearchBar searchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
     private MapView mapView;
     private AMap aMap;
     private AMapLocationClient mapLocationClient;
     private LocationSource.OnLocationChangedListener locationChangedListener;
     private AMapLocationClientOption mapLocationClientOption;
-    private static int i = 1;
+    private List<String> lastSearches;
+    private MaterialSearchBar searchBar;
+    
+setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId())
+            {
+                case R.id.action_settings:
+                    break;
+            ...
+            }
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        mapView = (MapView) findViewById(R.id.map_view);
 
-//        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+                    SearchHistoryProvider.AUTHORITY, SearchHistoryProvider.MODE);
+            suggestions.saveRecentQuery(query,null);
+        }
+
+        mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
 
         initMapView();
@@ -57,11 +94,11 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
     private void initMapView() {
         if (aMap == null) {
             aMap = mapView.getMap();
-            if (i > 0) {
+            if (mapLocationClient == null) {
                 LatLng haiKou = new LatLng(19.9816509, 110.3330756);
-                aMap.addMarker(new MarkerOptions().position(haiKou).title("出生地"));
+                aMap.addMarker(new MarkerOptions().position(haiKou).title("app出生地"));
                 aMap.moveCamera(CameraUpdateFactory.newLatLng(haiKou));
-                i--;
+                Toast.makeText(this, "定位中...", Toast.LENGTH_SHORT).show();
             }
             aMap.setMapType(AMap.MAP_TYPE_NORMAL);
             aMap.getUiSettings().setCompassEnabled(true);
@@ -70,7 +107,37 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
     }
 
     private void initUI() {
-//        findViewById(R.id.)
+
+            searchBar = findViewById(R.id.searchBar);
+            searchBar.setOnSearchActionListener(this);
+            //restore last queries from disk
+            lastSearches = loadSearchSuggestionFromDisk();
+            searchBar.setLastSuggestions(list);
+            //Inflate menu and setup OnMenuItemClickListener
+            searchBar.inflateMenu(R.menu.main);
+            searchBar.getMenu().setOnMenuItemClickListener(this);
+
+
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+        String s = enabled ? "enabled" : "disabled";
+        Toast.makeText(MapActivity.this, "Search " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+        startSearch(text.toString(), true, null, true);
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+        switch (buttonCode){
+            case MaterialSearchBar.BUTTON_NAVIGATION:
+                drawer.openDrawer(Gravity.LEFT);
+                break;
+        }
     }
 
     private void requestPermissions() {
@@ -114,8 +181,7 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
         aMap.setLocationSource(this);
         aMap.getUiSettings().setMyLocationButtonEnabled(true);
 
-        aMap.setTrafficEnabled(true);
-
+//        aMap.setTrafficEnabled(true);
         aMap.setMyLocationEnabled(true);
         mapLocationClient = new AMapLocationClient(getApplicationContext());
         mapLocationClient.setLocationListener(this);
@@ -128,9 +194,7 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
         mapLocationClientOption.setMockEnable(true);
         mapLocationClientOption.setInterval(1000);
         mapLocationClient.startLocation();
-
     }
-
 
     public void onLocationChanged(AMapLocation mapLocation) {
         if (locationChangedListener != null && mapLocation != null) {
@@ -162,7 +226,6 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
         }
     }
 
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -190,17 +253,27 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
             mapLocationClient = null;
             mapLocationClientOption = null;
         }
+        saveSearchHistoryToDisk(searchBar.getLastSuggestions());
     }
 
+    private void saveSearchHistoryToDisk(List<String> lastSearches) {
+        SharedPreferences searchHistory = this.getSharedPreferences("SearchHistory", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = searchHistory.edit();
 
-    public AMapLocationClient mlocationClient;
-    public AMapLocationListener mLocationListener = new AMapLocationListener() {
-        @Override
-        public void onLocationChanged(AMapLocation aMapLocation) {
+    }
 
-        }
-    };
+    public void clearSearchHostory() {
+        SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+                SearchHistoryProvider.AUTHORITY, SearchHistoryProvider.MODE);
+        suggestions.clearHistory();
+    }
 
+    private List<String> fetchSearchHistoryFromDisk() {
+        SharedPreferences searchHistoryPref = this.getSharedPreferences("SearchHistory", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = searchHistoryPref.edit();
+        String searchHistory = searchHistoryPref.getString("searchHistory", new String());
+
+    }
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -217,6 +290,37 @@ public class MapActivity extends AppCompatActivity implements CompoundButton.OnC
 
     }
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    protected void doSearchQuery() {
+        String keyWord =
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+        switch (buttonCode){
+            case MaterialSearchBar.BUTTON_NAVIGATION:
+                doSearch
+                break;
+            case MaterialSearchBar.BUTTON_SPEECH:
+                openVoiceRecognizer();
+        }
+    }
+searchBar.inflateMenu(R.menu.main);
+
+searchBar.getMenu().
+    public class SearchHistoryProvider extends SearchRecentSuggestionsProvider {
+        public final static String AUTHORITY = "com.djy.SearchHistoryProvider";
+        public final static int MODE = DATABASE_MODE_QUERIES;
+
+        public SearchHistoryProvider() {
+            setupSuggestions(AUTHORITY, MODE);
+        }
+    });
 //    protected void doSearchQuery() {
 //        keyWord = mSearchText.getText().toString().trim();
 //        currentPage = 0;
