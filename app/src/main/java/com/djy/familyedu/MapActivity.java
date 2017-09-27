67c865610358beb201805a85552daa988d781831
dong jiangying <krivergo3@gmail.com>
2017-09-27 20:22:25 +0800

    try more to add poi search follow AMAP and google demo, failed

--- app/src/main/java/com/djy/familyedu/MapActivity.java
+++ app/src/main/java/com/djy/familyedu/MapActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Toast;
@@ -27,6 +26,7 @@ import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import java.text.SimpleDateFormat;
@@ -34,9 +34,12 @@ import java.util.Date;
 import java.util.List;
 
 
-public class MapActivity extends AppCompatActivity implements AMapLocationListener
-        , CompoundButton.OnCheckedChangeListener, LocationSource {
+public abstract class MapActivity extends AppCompatActivity implements AMapLocationListener
+        , CompoundButton.OnCheckedChangeListener, LocationSource,PoiSearch.OnPoiSearchListener
+//        OnMarkerClickListener, InfoWindowAdapter, TextWatcher,
+//        , OnClickListener {
 
+{
     protected static final String TAG = "MapActivity";
     public AMapLocationClient mlocationClient;
     public AMapLocationListener mLocationListener = new AMapLocationListener() {
@@ -45,27 +48,16 @@ public class MapActivity extends AppCompatActivity implements AMapLocationListen
 
         }
     };
-    MaterialSearchBar searchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+//    MaterialSearchBar searchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
     private MapView mapView;
     private AMap aMap;
     private AMapLocationClient mapLocationClient;
     private LocationSource.OnLocationChangedListener locationChangedListener;
     private AMapLocationClientOption mapLocationClientOption;
-    private List<String> lastSearches;
-    private MaterialSearchBar searchBar;
-    
-setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-        @Override
-        public boolean onMenuItemClick(MenuItem item) {
-            switch (item.getItemId())
-            {
-                case R.id.action_settings:
-                    break;
-            ...
-            }
-            return false;
-        }
-    }
+
+    private List<String> searchHistory;
+//    private MaterialSearchBar searchBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,16 +65,22 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
-                    SearchHistoryProvider.AUTHORITY, SearchHistoryProvider.MODE);
-            suggestions.saveRecentQuery(query,null);
-        }
 
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
+/*        setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                                       @Override
+                                       public boolean onMenuItemClick(MenuItem item) {
+                                           switch (item.getItemId())
+                                           {
+                                               case R.id.clear_search_history:
+                                                   clearSearchHostory();
+                                                   break;
+                                           }
+                                           return false;
+                                       }
+                                   }*/
+initSearchIntent();
 
         initMapView();
         initUI();
@@ -91,6 +89,31 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
         startClientLocation();
     }
 
+    private String queryString;
+
+    private void initSearchIntent() {
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            queryString = intent.getStringExtra(SearchManager.QUERY);
+            doSearchQuery();
+        }
+    }
+    private PoiSearch.Query query;
+    private PoiSearch poiSearch;
+    protected void doSearchQuery() {
+
+        String keyWord = queryString;
+        query = new PoiSearch.Query(keyWord, "", "海口市");
+
+        poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(this);
+        poiSearch.searchPOIAsyn();
+    }
+
+//    SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+//            SearchHistoryProvider.AUTHORITY, SearchHistoryProvider.MODE);
+//            suggestions.saveRecentQuery(queryString,null);
+//}
     private void initMapView() {
         if (aMap == null) {
             aMap = mapView.getMap();
@@ -106,38 +129,19 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
         }
     }
 
+    private
     private void initUI() {
-
             searchBar = findViewById(R.id.searchBar);
-            searchBar.setOnSearchActionListener(this);
+//            searchBar.setOnSearchActionListener(this);
             //restore last queries from disk
             lastSearches = loadSearchSuggestionFromDisk();
             searchBar.setLastSuggestions(list);
             //Inflate menu and setup OnMenuItemClickListener
             searchBar.inflateMenu(R.menu.main);
             searchBar.getMenu().setOnMenuItemClickListener(this);
-
-
     }
+    private List<String> loadSearchSuggestionFromDisk() {
 
-    @Override
-    public void onSearchStateChanged(boolean enabled) {
-        String s = enabled ? "enabled" : "disabled";
-        Toast.makeText(MapActivity.this, "Search " + s, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onSearchConfirmed(CharSequence text) {
-        startSearch(text.toString(), true, null, true);
-    }
-
-    @Override
-    public void onButtonClicked(int buttonCode) {
-        switch (buttonCode){
-            case MaterialSearchBar.BUTTON_NAVIGATION:
-                drawer.openDrawer(Gravity.LEFT);
-                break;
-        }
     }
 
     private void requestPermissions() {
@@ -196,7 +200,39 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
         mapLocationClient.startLocation();
     }
 
-    public void onLocationChanged(AMapLocation mapLocation) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        if (mapLocationClient != null) {
+            mapLocationClient.onDestroy();
+            mapLocationClient = null;
+            mapLocationClientOption = null;
+        }
+        saveSearchHistoryToDisk(searchBar.getLastSuggestions());
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    private void saveSearchHistoryToDisk(List<String> lastSearches) {
+        SharedPreferences searchHistory = this.getSharedPreferences("SearchHistory", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = searchHistory.edit();
+
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+        String s = enabled ? "enabled" : "disabled";
+        Toast.makeText(MapActivity.this, "Search " + s, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+        startSearch(text.toString(), true, null, true);
+    }public void onLocationChanged(AMapLocation mapLocation) {
         if (locationChangedListener != null && mapLocation != null) {
             locationChangedListener.onLocationChanged(mapLocation);
             if (mapLocation.getErrorCode() == 0) {
@@ -227,9 +263,12 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
+    public void onButtonClicked(int buttonCode) {
+        switch (buttonCode){
+            case MaterialSearchBar.BUTTON_NAVIGATION:
+                drawer.openDrawer(Gravity.LEFT);
+                break;
+        }
     }
 
     @Override
@@ -239,27 +278,9 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-        if (mapLocationClient != null) {
-            mapLocationClient.onDestroy();
-            mapLocationClient = null;
-            mapLocationClientOption = null;
-        }
-        saveSearchHistoryToDisk(searchBar.getLastSuggestions());
-    }
-
-    private void saveSearchHistoryToDisk(List<String> lastSearches) {
-        SharedPreferences searchHistory = this.getSharedPreferences("SearchHistory", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = searchHistory.edit();
-
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 
     public void clearSearchHostory() {
@@ -295,10 +316,6 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
 
     }
 
-    @Override
-    protected void doSearchQuery() {
-        String keyWord =
-    }
 
     @Override
     public void onButtonClicked(int buttonCode) {
@@ -310,26 +327,30 @@ setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                 openVoiceRecognizer();
         }
     }
-searchBar.inflateMenu(R.menu.main);
 
-searchBar.getMenu().
-    public class SearchHistoryProvider extends SearchRecentSuggestionsProvider {
+public class SearchHistoryProvider extends SearchRecentSuggestionsProvider {
         public final static String AUTHORITY = "com.djy.SearchHistoryProvider";
         public final static int MODE = DATABASE_MODE_QUERIES;
 
         public SearchHistoryProvider() {
             setupSuggestions(AUTHORITY, MODE);
         }
-    });
+    }
+
+
+searchBar.inflateMenu(R.menu.main);
+
+searchBar.getMenu().
+    );
 //    protected void doSearchQuery() {
 //        keyWord = mSearchText.getText().toString().trim();
 //        currentPage = 0;
-//        query = new PoiSearch.Query(keyword, "", "海口市");
-//        query.setPageSize(12);
-//        query.setPageNum(currentPage);
+//        queryString = new PoiSearch.Query(keyword, "", "海口市");
+//        queryString.setPageSize(12);
+//        queryString.setPageNum(currentPage);
 //
 //        if (lp != null) {
-//            poiSearch = new PoiSearch(this, query);
+//            poiSearch = new PoiSearch(this, queryString);
 //            poiSearch.setOnPoiSearchListener(this);
 //            poiSearch.setBound(new PoiSearch.SearchBound(lp, 3500, true));
 //            poiSearch.searchPOIAsync();
